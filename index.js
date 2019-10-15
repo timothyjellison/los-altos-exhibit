b2747333a953cbc073f95199f45dceb3106e6842
Tim Ellison <timothyellison85@gmail.com>
2019-10-14 22:28:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const clipNames = [
+  'fire-audio-excerpt',
+  'police-audio-excerpt'
+];
 
+function setup(x, y) {
+  afterglow.on(clipNames[x],'play', function(){
+    const otherplayer = afterglow.getPlayer(clipNames[y]);
+    otherplayer.load();
+  });
+}
+
+setup(0, 1);
+setup(1, 0);
